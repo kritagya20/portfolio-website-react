6ff5549e386874b3c6a0757aefdb7a8d37da1640
Kritagya Singh Chouhan <kritagya2022@gmail.com>
2024-05-07 20:01:21 +0530

    Code of second version of the site.

--- v1/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export {default as BlogsPage} from './blogs_page/BlogsPage';
-export {default as Home} from './home/Home';
-export {default as ProjectArchives} from './project_archives/ProjectArchives';
