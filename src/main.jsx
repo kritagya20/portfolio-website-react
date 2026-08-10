20d3732acd304341809433b5a41817233caadb3f
kritagya20 <kritagya2022@gmail.com>
2026-08-11 00:04:42 +0530

    fix: Remove legacy SmoothCursor context component and main.jsx reference

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
-import  SmoothCursor  from './context/SmoothCursor.jsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <React.StrictMode>
-      <SmoothCursor />
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>
-  </>
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
